4476892c6019fcd6c32877ab2897153a0bc04ea9
koshtirushikesh <117973034+koshtirushikesh@users.noreply.github.com>
2023-02-03 15:43:52 +0530

    Refractor : To Switch Case

---
+++ EmployeeWageProblem/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class Employee
+    {
+        public int GetWorkingHours()
+        {
+            const int FullTime = 1;
+            const int PartTime = 2;
+            int WorkingHour;
+
+            Random random= new Random();
+            int check =random.Next(3);
+
+            switch(check)
+            {
+                case FullTime: WorkingHour = 8; break;
+                case PartTime: WorkingHour = 4; break;
+
+                default : WorkingHour = 0; break;
+            }
+            return WorkingHour;
+        }
+
+        public int CalculateDailyWage()
+        {
+            int WorkingHour = GetWorkingHours();
+            int WagePerHour = 20;
+            int DailyWage = 0;
+            DailyWage = WagePerHour * WorkingHour;
+            Console.WriteLine(" Wage is : " + DailyWage);
+            return DailyWage;
+        }
+
+
+    }
+}
