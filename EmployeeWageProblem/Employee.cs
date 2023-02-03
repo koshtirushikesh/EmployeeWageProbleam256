10c025d204be4ba2e97918d438a3711073bbdbfc
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2023-02-03 09:31:42 +0530

    Employee is Present or Absent

---
+++ EmployeeWageProblem/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class Employee
+    {
+        public void Attendance()
+        {
+            int present = 1;
+
+            Random random = new Random();
+            int check = random.Next(2);
+
+            if (check == present)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
